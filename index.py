aa3e427020aff655bafffc545ad51a411dac201f
shivdutt-B <shivdutt059@gmail.com>
2025-03-24 11:49:07 +0530

    init

--- index.py
+++ index.py
@@ -10,10 +10,8 @@ pygame.init()
 w = 1366
 h = 768
 
-icon = pygame.image.load("assets/icon.jpg")
 GD = pygame.display.set_mode((w, h), pygame.RESIZABLE)
 pygame.display.set_caption("Snakes N Ladders")
-pygame.display.set_icon(icon)
 pygame.display.update()
 
 # Graphics:
@@ -38,18 +36,10 @@ dice4 = pygame.image.load("assets/Dice4.png")
 dice5 = pygame.image.load("assets/Dice5.png")
 dice6 = pygame.image.load("assets/Dice6.png")
 
-redgoti = pygame.image.load("assets/redgoti.png")
-yellowgoti = pygame.image.load("assets/yellowgoti.png")
-greengoti = pygame.image.load("assets/greengoti.png")
-bluegoti = pygame.image.load("assets/bluegoti.png")
-menubg = pygame.image.load("assets/menu.jpg")
-p = pygame.image.load("assets/playbg.jpg")
-intbg = pygame.image.load("assets/intropic.png")
-intbg2 = pygame.image.load("assets/intropic2.jpg")
-intbg3 = pygame.image.load("assets/intropic3.jpg")
-intbg4 = pygame.image.load("assets/intropic4.jpg")
-intbg5 = pygame.image.load("assets/intropic5.jpg")
-credits1 = pygame.image.load("assets/credits.jpg")
+redpiece = pygame.image.load("assets/redpiece.png")
+yellowpiece = pygame.image.load("assets/yellowpiece.png")
+greenpiece = pygame.image.load("assets/greenpiece.png")
+bluepiece = pygame.image.load("assets/bluepiece.png")
 
 # mouse pos
 mouse = pygame.mouse.get_pos()
@@ -84,10 +74,10 @@ def text_objects1(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
-# Goti movement function
+# piece movement function
 
 
-def goti(a):
+def piece(a):
     l1 = [[406, 606], [456, 606], [506, 606], [556, 606], [606, 606], [656, 606], [706, 606], [756, 606], [806, 606], [856, 606], [906, 606], [906, 560], [856, 560], [806, 560], [756, 560], [706, 560], [656, 560], [606, 560], [556, 560], [506, 560], [456, 560], [456, 506], [506, 506], [556, 506], [606, 506], [656, 506], [706, 506], [756, 506], [806, 506], [856, 506], [906, 506], [906, 460], [856, 460], [806, 460], [756, 460], [706, 460], [656, 460], [606, 460], [556, 460], [506, 460], [456, 460], [456, 406], [506, 406], [556, 406], [606, 406], [656, 406], [706, 406], [756, 406], [806, 406], [856, 406], [
         906, 406], [906, 360], [856, 360], [806, 360], [756, 360], [706, 360], [656, 360], [606, 360], [556, 360], [506, 360], [456, 360], [456, 306], [506, 306], [556, 306], [606, 306], [656, 306], [706, 306], [756, 306], [806, 306], [856, 306], [906, 306], [906, 260], [856, 260], [806, 260], [756, 260], [706, 260], [656, 260], [606, 260], [556, 260], [506, 260], [456, 260], [456, 206], [506, 206], [556, 206], [606, 206], [656, 206], [706, 206], [756, 206], [806, 206], [856, 206], [906, 206], [906, 160], [856, 160], [806, 160], [756, 160], [706, 160], [656, 160], [606, 160], [556, 160], [506, 160], [456, 160]]
     l2 = l1[a]
@@ -167,25 +157,9 @@ def dice(a):
         GD.blit(a, (300, 500))
         pygame.display.update()
 
-# for mute and unmute
-
-
-def button2(text, xmouse, ymouse, x, y, w, h, i, a, fs):
-    global GD  # Add global declaration
-    # mouse pos
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if x+w > xmouse > x and y+h > ymouse > y:
-        pygame.draw.rect(GD, a, [x-2.5, y-2.5, w+5, h+5])
-        if pygame.mouse.get_pressed() == (1, 0, 0):
-            return True
-
-    else:
-        pygame.draw.rect(GD, i, [x, y, w, h])
-    message_display(text, (x+w+x)/2, (y+h+y)/2, fs)
-
-
 # Buttons for playing:
+
+
 def button1(text, xmouse, ymouse, x, y, w, h, i, a, fs):
     global GD  # Add global declaration
     # mouse pos
@@ -277,56 +251,6 @@ def button(text, xmouse, ymouse, x, y, w, h, i, a, fs, b):
     # main()
     # pygame.display.update()
 
-def intro():
-    global GD  # Add global declaration
-    time = pygame.time.get_ticks()
-    while pygame.time.get_ticks()-time < 2500:
-        GD.blit(intbg, (0, 0))
-        pygame.display.update()
-    while True:
-        time = pygame.time.get_ticks()
-        while pygame.time.get_ticks()-time < 500:
-            GD.blit(intbg2, (0, 0))
-            pygame.display.update()
-        time = pygame.time.get_ticks()
-        while pygame.time.get_ticks()-time < 500:
-            GD.blit(intbg3, (0, 0))
-            pygame.display.update()
-        time = pygame.time.get_ticks()
-        while pygame.time.get_ticks()-time < 500:
-            GD.blit(intbg4, (0, 0))
-            pygame.display.update()
-        time = pygame.time.get_ticks()
-        while pygame.time.get_ticks()-time < 500:
-            GD.blit(intbg5, (0, 0))
-            pygame.display.update()
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                return
-        pygame.display.update()
-
-
-def credit():
-    global GD  # Add global declaration
-    while True:
-        GD.blit(credits1, (0, 0))
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    Quit()
-            if event.type == pygame.VIDEORESIZE:
-                GD = handle_resize(event)
-
-        # mouse pos
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-        if button("Back", mouse[0], mouse[1], w/2-100, 700, 200, 50, red, b_red, 25, 20):
-            main()
-
-        pygame.display.update()
-
-
 # Handle window resizing
 def handle_resize(event):
     global w, h
@@ -353,7 +277,8 @@ def main():
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
-        GD.blit(menubg, (0, 0))
+        # Fill with white background instead of using the menubg image
+        GD.fill(white)
         button("Play", mouse[0], mouse[1], (w/2-100),
                h/2, 200, 100, green, b_green, 60, 1)
         button("Quit", mouse[0], mouse[1], (w/2-100),
@@ -381,7 +306,8 @@ def options():
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         b1 = b2 = b3 = b4 = b5 = -1
-        GD.blit(menubg, (0, 0))
+        # Fill with white background instead of using the menubg image
+        GD.fill(white)
         # Single player button - changed to blue
         b1 = button("Single Player", mouse[0], mouse[1],
                     (w/2-150), 250, 300, 50, blue, (0, 0, 255), 30, "s")
@@ -418,19 +344,20 @@ def play(b):
     time = 3000
     if b6 == 7:
         options()
-    GD.blit(p, (0, 0))
+    # Fill with white background instead of using the playbg2 image
+    GD.fill(white)
     GD.blit(board, (w/2-250, h/2-250))
     xcr = xcy = xcg = xcb = 406-25
     ycr = ycy = ycg = ycb = 606-25
-    GD.blit(redgoti, (xcy, ycy))
+    GD.blit(redpiece, (xcy, ycy))
     if 5 > b > 1 or b == 21:
-        GD.blit(yellowgoti, (xcy, ycy))
+        GD.blit(yellowpiece, (xcy, ycy))
 
     if 5 > b > 2 or b == 21:
-        GD.blit(greengoti, (xcg, ycg))
+        GD.blit(greenpiece, (xcg, ycg))
 
     if 5 > b > 2:
-        GD.blit(bluegoti, (xcb, ycb))
+        GD.blit(bluepiece, (xcb, ycb))
     p1 = "Player 1"
     p1score = 0
     if b == 21:
@@ -451,7 +378,8 @@ def play(b):
         l = False
         s = False
         time = 3000
-        GD.blit(p, (0, 0))
+        # Fill with white background instead of using the playbg2 image
+        GD.fill(white)
         GD.blit(board, (w/2-250, h/2-250))
         mouse = pygame.mouse.get_pos()
 
@@ -471,7 +399,7 @@ def play(b):
                     p1score, l, s, six = turn(p1score, l, s)
                     if not six:
                         t += 1
-                    xcr, ycr = goti(p1score)
+                    xcr, ycr = piece(p1score)
                     if p1score == 100:
                         time = pygame.time.get_ticks()
                         while pygame.time.get_ticks()-time < 2000:
@@ -490,7 +418,7 @@ def play(b):
                         700, 200, 50, yellow, (200, 200, 0), 30)
                 # Computer automatically plays
                 p2score, l, s, six = turn(p2score, l, s)
-                xcy, ycy = goti(p2score)
+                xcy, ycy = piece(p2score)
                 if not six:
                     t += 1
                     if b < 3 or b == 21:
@@ -514,7 +442,7 @@ def play(b):
                     p1score, l, s, six = turn(p1score, l, s)
                     if not six:
                         t += 1
-                    xcr, ycr = goti(p1score)
+                    xcr, ycr = piece(p1score)
                     if p1score == 100:
                         time = pygame.time.get_ticks()
                         while pygame.time.get_ticks()-time < 2000:
@@ -531,7 +459,7 @@ def play(b):
             if t == 2:
                 if button1("Player 2", mouse[0], mouse[1], 400, 700, 200, 50, yellow, (200, 200, 0), 30):
                     p2score, l, s, six = turn(p2score, l, s)
-                    xcy, ycy = goti(p2score)
+                    xcy, ycy = piece(p2score)
                     if not six:
                         t += 1
                         if b < 3:
@@ -553,7 +481,7 @@ def play(b):
             if t == 3:
                 if button1("Player 3", mouse[0], mouse[1], 700, 700, 200, 50, green, b_green, 30):
                     p3score, l, s, six = turn(p3score, l, s)
-                    xcg, ycg = goti(p3score)
+                    xcg, ycg = piece(p3score)
                     if not six:
                         t += 1
                         if b < 4:
@@ -575,7 +503,7 @@ def play(b):
             if t == 4:
                 if button1("Player 4", mouse[0], mouse[1], 1000, 700, 200, 50, blue, (0, 0, 255), 30):
                     p4score, l, s, six = turn(p4score, l, s)
-                    xcb, ycb = goti(p4score)
+                    xcb, ycb = piece(p4score)
                     if not six:
                         t += 1
                         if b < 5:
@@ -594,15 +522,15 @@ def play(b):
 
         b6 = button("Back", mouse[0], mouse[1], 0,
                     0, 200, 50, red, b_red, 30, 7)
-        GD.blit(redgoti, (xcr, ycr))
+        GD.blit(redpiece, (xcr, ycr))
         if 5 > b > 1 or b == 21:
-            GD.blit(yellowgoti, (xcy+2, ycy))
+            GD.blit(yellowpiece, (xcy+2, ycy))
 
         if 5 > b > 2:
-            GD.blit(greengoti, (xcg+4, ycg))
+            GD.blit(greenpiece, (xcg+4, ycg))
 
         if 5 > b > 3:
-            GD.blit(bluegoti, (xcb+6, ycb))
+            GD.blit(bluepiece, (xcb+6, ycb))
 
         if l:
             time = pygame.time.get_ticks()
